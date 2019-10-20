d53492d0bb3905915dddea1b9f8babb069be798d
feng23010 <fy_6321750@163.com>
2019-10-21 01:13:25 +0800

    更新

--- Zol/Zol/spiders/Zolspider.py
+++ Zol/Zol/spiders/Zolspider.py
@@ -9,22 +9,37 @@ class ZolspiderSpider(scrapy.Spider):
     part_url='http://detail.zol.com.cn'
     def parse(self, response):
 
-        pplist=response.xpath(".//div[@class='brand-list']/a/@href").extract()
-        for ppl in pplist:
-            ppl=ZolspiderSpider.part_url+ppl
+        pplist=response.xpath(".//div[@class='brand-list']/a")
+
+        for a_list in pplist:
+            item = {}
+            item["ppname"]=a_list.xpath("./text()").extract_first()
+            item["ppurl"]=ZolspiderSpider.part_url+a_list.xpath("./@href").extract_first()
             yield scrapy.Request(
-                ppl,
+                item["ppurl"],
                 callback=self.parse_phone_list,
-                meta={"item":ppl}
-            )
+                meta={"item":item}
+                )
+            #print(item)
 
     def parse_phone_list(self,response):
-        phone_list_url=response.meta["item"]
-        print(phone_list_url)
-        for list in phone_list_url:
-            item={}
-            phone_li_list=response.xpath(".//ul[@id='J_PicMode']/li[starts-with(@data-follow-id,'p')]")
-            for phone_li in phone_li_list:
-                item["phone_name"]=phone_li.xpath("./h3/a/text()").extract_first()
-                item["phone_url"]=phone_li.xpath("./a/@href").extract_first()
-                print (item)
+        item=response.meta["item"]
+        phone_li_list=response.xpath(".//ul[@id='J_PicMode']/li[starts-with(@data-follow-id,'p')]")
+        for phone_li in phone_li_list:
+            item["phone_name"]=phone_li.xpath("./h3/a/text()").extract_first()
+            item["phone_url"]=ZolspiderSpider.part_url+"/"+str((int((phone_li.xpath("./a/@href").extract_first()[17:24])[0:4])+1))+"/"+phone_li.xpath("./a/@href").extract_first()[17:24]+"/param.shtml"
+            yield scrapy.Request(
+                item["phone_url"],
+                callback=self.parse_phone_content,
+                meta={"item": item}
+            )
+        print(item)
+    def parse_phone_content(self,response):
+        item=response.meta["item"]
+        #print(item)
+        # if response.xpath(".//div[@class='detailed-parameters']/table[1]/tr[2]/td/span/text()").extract_first() is not None:
+        #     item["shangshishijian"]=response.xpath(".//div[@class='detailed-parameters']/table[1]/tr[2]/td/span/text()").extract_first()
+        # else:
+        #     item["shangshishijian"]=response.xpath(".//div[@class='detailed-parameters']/table[1]/tr[2]/td/span/a/text()").extract_first()
+        #
+        # print(item)
