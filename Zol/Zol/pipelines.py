495ead95e2c20f57fa4485ff7ca099f46b09383c
fengyue23010 <fy_6321750@163.com>
2019-10-21 18:06:37 +0800

    gx

--- Zol/Zol/pipelines.py
+++ Zol/Zol/pipelines.py
@@ -4,8 +4,11 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
 
 class ZolPipeline(object):
     def process_item(self, item, spider):
+        with open("iphone.txt", "a", encoding="utf-8") as f:
+            f.write(json.dumps(item, ensure_ascii=False))
+            f.write("\n")
         return item
--- Zol/Zol/spiders/Zolspider.py
+++ Zol/Zol/spiders/Zolspider.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import requests
-
+from copy import deepcopy
 class ZolspiderSpider(scrapy.Spider):
     name = 'Zolspider'
     allowed_domains = ['zol.com.cn']
@@ -18,12 +17,13 @@ class ZolspiderSpider(scrapy.Spider):
             yield scrapy.Request(
                 item["ppurl"],
                 callback=self.parse_phone_list,
-                meta={"item":item}
+                meta={"item":deepcopy(item)}
                 )
-            #print(item)
+
 
     def parse_phone_list(self,response):
-        item=response.meta["item"]
+        item=deepcopy(response.meta["item"])
+
         phone_li_list=response.xpath(".//ul[@id='J_PicMode']/li[starts-with(@data-follow-id,'p')]")
         for phone_li in phone_li_list:
             item["phone_name"]=phone_li.xpath("./h3/a/text()").extract_first()
@@ -31,15 +31,24 @@ class ZolspiderSpider(scrapy.Spider):
             yield scrapy.Request(
                 item["phone_url"],
                 callback=self.parse_phone_content,
-                meta={"item": item}
-            )
-        print(item)
+                meta={"item":deepcopy(item)}
+                )
+            next_page=phone_li.xpath(".//div[@class='pagebar']/a[@class='next']/@href")
+						if next_page is not None:
+							 yield scrapy.Request(
+                item["next_page"],
+                callback=self.parse_phone_list,
+                meta={"item":deepcopy(item)}
+                )
     def parse_phone_content(self,response):
-        item=response.meta["item"]
-        #print(item)
-        # if response.xpath(".//div[@class='detailed-parameters']/table[1]/tr[2]/td/span/text()").extract_first() is not None:
-        #     item["shangshishijian"]=response.xpath(".//div[@class='detailed-parameters']/table[1]/tr[2]/td/span/text()").extract_first()
-        # else:
-        #     item["shangshishijian"]=response.xpath(".//div[@class='detailed-parameters']/table[1]/tr[2]/td/span/a/text()").extract_first()
-        #
-        # print(item)
+        item = deepcopy(response.meta["item"])
+        if response.xpath(".//div[@class='detailed-parameters']/table[1]/tr[2]/td/span/a/text()").extract_first() is not None:
+            if response.xpath(".//div[@class='detailed-parameters']/table[1]/tr[2]/td/span/a/text()").extract_first() =='预约抢购':
+                item["Time to market"] = response.xpath(".//div[@class='detailed-parameters']/table[1]/tr[3]/td/span/text()").extract()
+            else:
+               item["Time to market"]=response.xpath(".//div[@class='detailed-parameters']/table[1]/tr[2]/td/span/a/text()").extract_first()
+        else:
+           item["Time to market"]=response.xpath(".//div[@class='detailed-parameters']/table[1]/tr[2]/td/span/text()").extract_first()
+
+        yield item
+
