c1fc17d87835cc79814b2f779e2b90205c76abd3
feng23010 <fy_6321750@163.com>
2019-10-22 01:29:50 +0800

    更新

--- Zol/Zol/pipelines.py
+++ Zol/Zol/pipelines.py
@@ -6,9 +6,14 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import json
 import pandas as pd
+from copy import deepcopy
+import csv
 class ZolPipeline(object):
     def process_item(self, item, spider):
-        with open("iphone.txt", "a", encoding="utf-8") as f:
+        # data_csv_ar=[]
+        # data_csv_ar.append(deepcopy(item))
+        # print(data_csv_ar)
+        with open("phone.txt", "a", encoding="utf-8") as f:
             f.write(json.dumps(item, ensure_ascii=False))
             f.write("\n")
         return item
